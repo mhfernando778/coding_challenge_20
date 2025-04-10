6c1d1f3e44ee680d83d46f3bce9257e795aa538c
Fernando <mhfernando778@gmail.com>
2025-04-10 18:03:53 -0400

    Setup root component with state and props structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; // Importing React and the useState hook from the React library
 import Gallery from './Gallery';
 
-function App() {
+function App() { // Defining the App component
   const [tours, setTours] = useState([]);
 
-  const addTour = (newTour) => {
+  const addTour = (newTour) => { // Function to add a new tour to the list
     setTours((prevTours) => [...prevTours, newTour]);
   };
 
@@ -12,7 +12,7 @@ function App() {
     setTours((prevTours) => prevTours.filter((tour) => tour.id !== id));
   };
 
-  return (
+  return ( // Rendering the App component
     <div>
       <h1>Gallery</h1>
       <Gallery tours={tours} addTour={addTour} removeTour={removeTour} />
@@ -20,4 +20,5 @@ function App() {
   );
 };
 
+// Exporting the App component as the default export of this file
 export default App;
